01f1b981a04be11048a58ce92971eddb979fb2e4
Jeff Kilroy <jeffkilroy@gmail.com>
2019-10-12 14:40:39 -0400

    banner

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -12,14 +12,14 @@
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="@yield('metaTitle',config('app.name'))" />
-    <meta property="og:description" content="@yield('metaDescription','...')" />
+    <meta property="og:description" content="@yield('metaDescription','Work Together. Get Paid Together. With Nano!')" />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta property="og:site_name" content="{{ config('app.name') }}" />
     <meta property="og:image" content="@yield('metaImage',asset('/img/banner-twitter.png'))" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@kilrizzy" />
     <meta name="twitter:title" content="@yield('metaTitle',config('app.name'))" />
-    <meta name="twitter:description" content="@yield('metaDescription','...')" />
+    <meta name="twitter:description" content="@yield('metaDescription','Work Together. Get Paid Together. With Nano!')" />
     <meta name="twitter:image" content="@yield('metaImage',asset('/img/banner-twitter.png'))" />
     <meta name="theme-color" content="#3182ce">
 </head>
