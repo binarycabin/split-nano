02cbf58ddfc5484c1c58b0e227dfd89913ddb406
Jeff Kilroy <jeffkilroy@gmail.com>
2019-10-13 11:36:09 -0400

    Add github to footer

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-blue-900">
     <div class="container mx-auto px-2 py-12 text-blue-400 opacity-75 text-center md:text-right text-sm">
-        &copy; {{ date('Y') }} {{ config('app.name') }} by <a href="{{ url('https://twitter.com/kilrizzy') }}" target="_blank" class="text-blue no-underline hover:underline">@kilrizzy</a>
+        &copy; {{ date('Y') }} {{ config('app.name') }} by <a href="{{ url('https://twitter.com/kilrizzy') }}" target="_blank" class="text-blue no-underline hover:underline">@kilrizzy</a> | <a href="https://github.com/binarycabin/split-nano" target="_blank">View on Github</a>
     </div>
 </footer>
